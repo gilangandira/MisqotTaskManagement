d3ae583e0b0c35dc26a53b389d329b664b7a61ee
Gilang Andy <gilangandy24@gmail.com>
2023-12-12 02:16:48 +0700

    Performance Fixed

--- app/Console/Commands/PerformanceCounter.php
+++ app/Console/Commands/PerformanceCounter.php
@@ -6,8 +6,9 @@ use Carbon\Carbon;
 use App\Models\User;
 use App\Models\Performance;
 use Illuminate\Console\Command;
+use App\Helpers\ResponseFormatter;
 
-class PerformanceMonth extends Command
+class PerformanceCounter extends Command
 {
     /**
      * The name and signature of the console command.
@@ -32,26 +33,23 @@ class PerformanceMonth extends Command
     {
         $users = User::all();
         $month = Carbon::now()->month;
-        // Loop melalui setiap pengguna
+
         foreach ($users as $user) {
-            // Hitung jumlah tugas yang diselesaikan oleh pengguna berdasarkan status
             $tasksCompleted = $user->tasks()
-                ->where('status_id', '=', 1) // Gantilah '1' dengan ID status yang sesuai
-                ->whereMonth('dates', '=', $month) // Gantilah $bulan dengan bulan yang diinginkan (misalnya, 1 untuk Januari, 2 untuk Februari, dst.)
-                ->count(); // Gantilah '1' dengan ID status yang sesuai
+                ->where('status_id', '=', 1)
+                ->whereMonth('dates', '=', $month)
+                ->count();
 
-            // Simpan data ke dalam tabel 'performance'
-
-
-
-            $task = Performance::create([
+            Performance::create([
                 'user_id' => $user->id,
-                'month' => $month, // Gunakan bulan saat ini sebagai contoh, Anda mungkin ingin menyesuaikannya
+                'month' => $month,
                 'total' => $tasksCompleted,
             ]);
-
-            echo $task + "Has Created";
         }
 
+        $this->info('Performance data has been updated successfully.');
+
+        // Alternatively, you can log the information
+        // \Log::info('Performance data has been updated successfully.');
     }
 }
